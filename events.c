bc3952d0032fc9a7cc39ce5a3c76d5884badc699
Hardy Jimenez <hardy.jimenez.matos@accenture.com>
2022-11-04 18:15:36 +0100

    all

--- events.c
+++ events.c
@@ -1,10 +1,16 @@
 #include "so_long.h"
-int			key_input_hook(int keycode, void *param)
+
+int	ft_update_map(int keycode, t_map *map)
 {
-  printf("Hello World\nThere's been a key input with %c", keycode);
-  if (!param)
-	printf("param");
-  else
-	printf("noParam");
+  if(!map)
+	exit (0);
+  printf("%d\n", keycode);
+  return (0);
+}
+
+int	key_input_hook(int keycode, void *param)
+{
+  ft_update_map(keycode, param);
+  // ft_put_images_to_map(param);
   return (0);
 }
